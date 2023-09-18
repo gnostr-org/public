617546802de00a64d39e9eb3c01bf38c4c36967c
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-09-18 13:29:49 -0400

    create.mjs: naming ndkEvent_kinds/types

--- create.mjs
+++ create.mjs
@@ -24,35 +24,47 @@ var image = fs.readFileSync('./public/ostriches.jpeg', 'utf8')
 console.log(page)
 console.log(js)
 console.log(css)
-//console.log(image)
+console.log(image)
 
 
-//const ndkEvent = new NDKEvent(ndk);
-//ndkEvent.kind = 5394;
-//ndkEvent.content = js;
-//let jsEvent = await ndkEvent.publish(); // This will trigger the extension to ask the user to confirm signing.
+const ndkEvent_js = new NDKEvent(ndk);
+ndkEvent_js.kind = 5394;
+ndkEvent_js.content = js;
+let jsEvent = await ndkEvent_js.publish(); // This will trigger the extension to ask the user to confirm signing.
 
-//console.log(jsEvent) // -> 4885034c358f0f3e57bfa3018685801e49d4a384c828c6ad0f384fbacd19d941
-
-
-
-//const ndkEvent = new NDKEvent(ndk);
-//ndkEvent.kind = 5393;
-//ndkEvent.content = css;
-//let jsEvent = await ndkEvent.publish(); // This will trigger the extension to ask the user to confirm signing.
-
-//console.log(jsEvent) // -> 7ccda059f9f4b7ddfe8e39aa4f3a41f2c262bfee9203b5894eff36a8f9499a05
+console.log(jsEvent) // -> 4885034c358f0f3e57bfa3018685801e49d4a384c828c6ad0f384fbacd19d941
 
 
 
-const ndkEvent = new NDKEvent(ndk);
-ndkEvent.kind = 5392;
-ndkEvent.content = page;
-let jsEvent = await ndkEvent.publish(); // This will trigger the extension to ask the user to confirm signing.
+const ndkEvent_css = new NDKEvent(ndk);
+ndkEvent_css.kind = 5393;
+ndkEvent_css.content = css;
+let jsEvent = await ndkEvent_css.publish(); // This will trigger the extension to ask the user to confirm signing.
+
+console.log(jsEvent) // -> 7ccda059f9f4b7ddfe8e39aa4f3a41f2c262bfee9203b5894eff36a8f9499a05
+
+
+
+const ndkEvent_page = new NDKEvent(ndk);
+ndkEvent_page.kind = 5392;
+ndkEvent_page.content = page;
+let jsEvent = await ndkEvent_page.publish(); // This will trigger the extension to ask the user to confirm signing.
 
 console.log(jsEvent) // -> 1efc13c6ffbaf60c0347baf89f6ecaad22f74abf82165fcdb55ef7e8cca8a597
 
 
+const ndkEvent_image_1064 = new NDKEvent(ndk);
+ndkEvent_image_1064.kind = 1064;
+ndkEvent_image_1064.content = image;
+let jsEvent = await ndkEvent_image_1064.publish(); // This will trigger the extension to ask the user to confirm signing.
 
-// Image -> 3c55892674bd88431fd0d9b611e96e65c91802a128596bf3bcd6ca6c4aa2d5c7
+console.log(jsEvent) // Image -> 3c55892674bd88431fd0d9b611e96e65c91802a128596bf3bcd6ca6c4aa2d5c7
+
+const ndkEvent_1065 = new NDKEvent(ndk);
+ndkEvent_1065.kind = 1065;
+ndkEvent_1065.content = image;
+let jsEvent = await ndkEvent_1065.publish(); // This will trigger the extension to ask the user to confirm signing.
+
+console.log(jsEvent) // Image -> 3c55892674bd88431fd0d9b611e96e65c91802a128596bf3bcd6ca6c4aa2d5c7
+
 
